119e3f8bbfe979baf22947f3142f92ec113bdd0a
truongxuan19022000 <xuantruong19022000@gmail.com>
2022-01-26 16:56:20 +0700

    generate token to use category function

---
+++ PHP/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category_name' => 'required',
+            'category_desc' => 'required',
+            'category_status' => 'required',
+        ]);
+        return Category::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  $id)
+    {
+        $category = Category::find($id);
+        $category ->update($request->all());
+        return response('Update successfully',200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category=Category::destroy($id);
+        return response('Delete successfully!',200);
+    }
+}
--- PHP/routes/api.php
+++ PHP/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +16,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// punlic routes
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/categories',[CategoryController::class,'index']);
+Route::get('/categories/{id}',[CategoryController::class,'show']);
+Route::post('/categories',[CategoryController::class,'store']);
+Route::put('/categories/{id}',[CategoryController::class,'update']);
+Route::delete('/categories/{id}',[CategoryController::class,'destroy']);
+//protechted route 
+Route::group(['middleware' => ['auth:sanctum']],function(){
+    Route::post('/categories',[CategoryController::class,'store']);
+    Route::put('/categories/{id}',[CategoryController::class,'update']);
+    Route::delete('/categories/{id}',[CategoryController::class,'destroy']);
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
